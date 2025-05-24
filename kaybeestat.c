f2c0e51b2baa561b771a5cdd218b974134b84fa4
Simon Slamka <simon.slamka@ongakken.com>
2025-05-24 13:41:16 +0300

    populate the typedef struct; print key and timestamp

--- kaybeestat.c
+++ kaybeestat.c
@@ -1,16 +1,31 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/ktime.h>
 #include <linux/input.h>
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Simon Slamka of Ongakken");
+MODULE_AUTHOR("Simon Slamka of Ongakken Corp.");
 MODULE_DESCRIPTION("KayBeeStat: a keyboard input event stat module for enthusiasts (not for long-term use!!)");
 MODULE_VERSION("0.1");
+
+#define KAYBEESTAT_MAX_EVENTS 32
 
 static int kaybeestat_connect(struct input_handler *handler, struct input_dev *dev, const struct input_device_id *id);
 static void kaybeestat_disconnect(struct input_handle *handle);
 static void kaybeestat_event(struct input_handle *handle, unsigned int type, unsigned int code, int value);
+
+typedef struct
+{
+    uint8_t keycode;
+    uint64_t timestamp;
+    uint8_t pressed;
+    uint8_t released;
+    char padding[54];
+} __attribute__((aligned(64))) keystroke_event;
+
+static keystroke_event kaybeestat_events[KAYBEESTAT_MAX_EVENTS];
+static size_t kaybeestat_idx = 0;
 
 static const struct input_device_id kaybeestat_ids[] =
     {
@@ -35,16 +50,19 @@ static DEFINE_SPINLOCK(kaybeestat_lock);
 
 static void kaybeestat_event(struct input_handle *handle, unsigned int type, unsigned int code, int value)
 {
-    if (unlikely(type != EV_KEY)) return;
+    if (unlikely(type != EV_KEY || value == 2)) { return; }
 
-    if (likely(value == 1))
-    {
-        spin_lock(&kaybeestat_lock);
+    spin_lock(&kaybeestat_lock);
 
-        printk(KERN_INFO "KayBeeStat: key pressed: %u\n", code);
+    kaybeestat_events[kaybeestat_idx].keycode = code;
+    kaybeestat_events[kaybeestat_idx].timestamp = ktime_get_ns();
+    kaybeestat_events[kaybeestat_idx].pressed = (value == 1);
+    kaybeestat_events[kaybeestat_idx].released = (value == 0);
+    memset(kaybeestat_events[kaybeestat_idx].padding, 0, sizeof(kaybeestat_events[kaybeestat_idx].padding));
+    printk(KERN_INFO "KayBeeStat: key %s: %u (at %llu ns)\n", value ? "pressed" : "released", code, kaybeestat_events[kaybeestat_idx].timestamp);
+    kaybeestat_idx = (kaybeestat_idx + 1) % KAYBEESTAT_MAX_EVENTS;
 
-        spin_unlock(&kaybeestat_lock);
-    }
+    spin_unlock(&kaybeestat_lock);
 }
 
 static int kaybeestat_connect(struct input_handler *handler, struct input_dev *dev, const struct input_device_id *id)
